6881db5f4f40b3f536ad048916fd95fc23d5eef9
jinwoo <jinwoo@example.com>
2026-04-15 14:35:14 +0900

    commit1

---
+++ main.py
@@ -0,0 +1,9 @@
+student_id = "20233314"
+name = "이진우"
+english_name = "jinwoo"
+
+print("AI Engineering Student")
+touch main.py test.py
+mkdir data model
+touch data/train.csv data/test.csv
+touch model/model.pkl
