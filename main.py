56cd848abfa758aabc823d4c532721f1ae3bc760
jinwoo <jinwoo@example.com>
2026-04-29 14:35:08 +0900

    jinwoo1

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-student_id = "20233314"
-name = "이진우"
-english_name = "jinwoo"
-
-print("AI Engineering Student")
-touch main.py test.py
-mkdir data model
-touch data/train.csv data/test.csv
-touch model/model.pkl
+print('1')
